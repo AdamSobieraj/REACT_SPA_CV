8a315f116da511eaa163793c4de3e0c841fa3af0
Tor.com <adam.sobieraj@op.pl>
2026-09-13 18:58:06 +0200

    [000002] - host on git

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/REACT_SPA_CV/',
   plugins: [react()],
   build: {
     rollupOptions: {
